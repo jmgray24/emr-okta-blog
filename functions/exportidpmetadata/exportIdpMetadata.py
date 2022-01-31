f9d07eb18d0e7540f77e25e654a5f0c4cb00f7e8
Folkes <samfolke@08f8bc67955b.ant.amazon.com>
2022-01-31 16:24:18 -0600

    added ability to setup lf from custom resource

--- functions/exportidpmetadata/exportIdpMetadata.py
+++ functions/exportidpmetadata/exportIdpMetadata.py
@@ -1,11 +1,18 @@
 import urllib
 import urllib.request
-import boto3
 import urllib.parse
 from crhelper import CfnResource
 import zipfile 
 from datetime import *
 from io import BytesIO
+import sys
+from pip._internal import main
+
+main(['install', '-I', '-q', 'boto3', '--target', '/tmp/', '--no-cache-dir', '--disable-pip-version-check'])
+sys.path.insert(0,'/tmp/')
+
+import boto3
+from botocore.exceptions import ClientError
 
 helper = CfnResource(
 	json_logging=False,
@@ -38,7 +45,18 @@ def upload_data(s3Client,dataBucketName):
         )
     print('File upload complete!')
 
+def setup_lf_perms(lf,sts):
+    print('Enabling Lake Formations perms...')
+    lf_settings = lf.get_data_lake_settings()['DataLakeSettings']
+    lf_settings['CreateDatabaseDefaultPermissions'] = []
+    lf_settings['CreateTableDefaultPermissions'] = []
+    lf_settings['AllowExternalDataFiltering'] = True
+    lf_settings['ExternalDataFilteringAllowList'] = [{'DataLakePrincipalIdentifier': sts.get_caller_identity().get('Account')}]
+    lf.put_data_lake_settings(DataLakeSettings=lf_settings)
+    print('Successfully enabled Lake Formation perms!')
+
 def handler(event, context):
+    print(boto3.__version__)
     helper(event, context)
 
 @helper.create
@@ -46,6 +64,9 @@ def create(event, context):
     s3 = boto3.resource('s3')
     s3Client = boto3.client('s3')
     iam = boto3.client('iam')
+    sts = boto3.client('sts')
+    lf = boto3.client('lakeformation')
+    setup_lf_perms(lf,sts)
     helper.Data['SAMLProviderArn'] = create_saml_idp(s3, iam, event['ResourceProperties']['DataBucketName'], event['ResourceProperties']['SAMLProviderName'], event['ResourceProperties']['OktaAppMetadataURL'])
     upload_data(s3Client,event['ResourceProperties']['DataBucketName'])
 
